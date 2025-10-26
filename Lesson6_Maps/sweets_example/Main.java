c64f823bb7cac828b64766d36d18d9b28cd34fc1
AlexTeder <tederest@gmail.com>
2025-10-26 22:35:25 +0200

    Lesson6 - Maps

---
+++ Lesson6_Maps/sweets_example/Main.java
@@ -0,0 +1,19 @@
+package Lesson6_Maps.sweets_example;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, List<String>> sweetByLetter = new HashMap<>();
+        sweetByLetter.put("A", List.of("Apple Pie", "Almond Cookie"));
+        sweetByLetter.put("C", List.of("Caramel", "Cheesecake", "Cotton Candy"));
+        sweetByLetter.put("G", List.of("Gummy bear", "Gingerbread"));
+        sweetByLetter.put("T", List.of("Tiramisu", "Toffee", "Truffles"));
+        sweetByLetter.put("M", List.of("Marshmallow", "Mocha", "Macaroon", "Milkshake"));
+
+        System.out.println(sweetByLetter);
+        System.out.println(sweetByLetter.get("C"));
+    }
+}
